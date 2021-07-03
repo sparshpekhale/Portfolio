26eefb37a2cb207b089f0edca17b8c03b729f899
Sparsh <sparsh8910@yahoo.com>
2021-07-03 21:10:28 +0530

    to production

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE')
-# DEBUG = False
+# DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'portfolio-sparsh.herokuapp.com']
 
