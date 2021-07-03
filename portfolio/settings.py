a7bf8c0cd9ea69903309b83e3157908ac7d2450d
Sparsh <sparsh8910@yahoo.com>
2021-07-04 01:29:55 +0530

    change url

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = os.environ.get('DEBUG_VALUE')
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'portfolio-sparsh.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'sparsh-pek.herokuapp.com']
 
 
 # Application definition
